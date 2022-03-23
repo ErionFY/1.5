d5b654f12b2a39272004149a00fec40afdc9d04a
Nikita Panfilov <npanfilov2003@gmail.com>
2022-03-24 00:18:43 +0700

    add searchPath function

--- Jtest_body.js
+++ Jtest_body.js
@@ -49,3 +49,42 @@ function paintingA()
     }
 
 }
+
+function searchPath(n, matrix, startPoint, finishPoint){
+    // В матрице: 0 - стена, 1 - проход
+    let queue=[];
+    let prevPoint=new Array(n*n);
+    prevPoint.fill(-1);
+    queue.push(startPoint);
+    let current;
+
+    while(queue.length==0){
+        current=queue.shift();
+        if(current=finishPoint){
+            break;
+        }
+        adjacentCells(current).forEach(function(nextPoint){
+            if(prevPoint[current]!=nextPoint && matrix[Math.floor(nextPoint/n)][nextPoint%n]==1){
+                queue.push(nextPoint);
+                prevPoint[nextPoint]=current;
+            }
+        });
+    }
+}
+
+function adjacentCells(point,n){
+    let array = [];
+    if (point%n!=0){
+        array.push(point-1);
+    }
+    if(point+1%n!=0){
+        array.push(point+1);
+    }
+    if(point>=n){
+        array.push(point-n);
+    }
+    if(point+n<n*n){
+        array.push(point+n);
+    }
+    return array;
+}
