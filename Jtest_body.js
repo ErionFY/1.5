cfbf7c8fda7562fae53c7f3ae93ab75155e30f35
ErionFY <goncharov.timofej@gmail.com>
2022-03-26 18:51:01 +0700

    main

--- Jtest_body.js
+++ Jtest_body.js
@@ -1,6 +1,6 @@
 
 var matrixA=new Array();
-
+// матрица, отображающая лабиринт 1-есть проход . 0-нет.
 var size =canvasA.height=901;
 canvasA.width=901;
 
@@ -9,8 +9,8 @@ var ctxA=cA.getContext("2d");
 
 
 var N;
-var startPoint={x:"",y:""};
-var finishPoint={x:"",y:""};
+var startPoint={x:-1,y:-1};
+var finishPoint={x:-1,y:-1};
 var typeA;
 var pxvalue
 
@@ -19,17 +19,9 @@ var pxvalue
 function clickedA(types)
 {
     typeA=types;
-    if(typeA==='startA'){
-       cA.addEventListener('click', position);//{"once":true}
-    }
-
-    if(typeA==='finishA'){
-       cA.addEventListener('click', position);//,{"once":true}
-    }
-
-    if(typeA==='impassable'){
-        cA.addEventListener('click', position,);
-     }
+    
+    cA.addEventListener('click', position);//{"once":true}
+     //считываем тип кнопки и переходим на соответствующий if  в функции position
 }
 
 
@@ -37,17 +29,16 @@ function clickedA(types)
 function position(event)
 {
  var x=event.offsetX;
- var y=event.offsetY;
+ var y=event.offsetY;//получаем координаты клика на canvas'e
  var Nx=Math.floor(x/pxvalue);
- var Ny=Math.floor(y/pxvalue);
+ var Ny=Math.floor(y/pxvalue);//получаем, клетку(координаты клетки)
  var colorSquare;
 if(typeA==='startA'){
 
-    if(Nx===finishPoint.x&&Ny===finishPoint.y)
-    {
+    if(Nx===finishPoint.x&&Ny===finishPoint.y){
         finishPoint.x=-1;
         finishPoint.y=-1;
-    }
+    }//если закрашиваем клетку , которая окрашена в финиш , то её координаты сбрасываются
 
     matrixA[Ny][Nx]=1;
     colorSquare='green';
@@ -59,11 +50,10 @@ if(typeA==='startA'){
 }
 else if(typeA==='finishA'){
 
-    if(startPoint.x===Nx&&startPoint.y===Ny)
-    {
+    if(startPoint.x===Nx&&startPoint.y===Ny){
         startPoint.x=-1;
         startPoint.y=-1;
-    }
+    }//если закрашиваем клетку , которая окрашена в старт , то её координаты сбрасываются
 
     matrixA[Ny][Nx]=1;
     colorSquare='red';
@@ -73,29 +63,24 @@ else if(typeA==='finishA'){
     ctxA.fillStyle=colorSquare;
     ctxA.fillRect(Nx*pxvalue+1,Ny*pxvalue+1,pxvalue-2,pxvalue-2)
 }
-else if(typeA==='impassable'&& matrixA[Ny][Nx]===1){
-    
-    if(Nx===finishPoint.x&&Ny===finishPoint.y)
-    {
+else if(typeA==='impassable'&& matrixA[Ny][Nx]===1){//если клетка проходима
+    if(Nx===finishPoint.x&&Ny===finishPoint.y){
         finishPoint.x=-1;
         finishPoint.y=-1;
     }
-    else if(Nx===startPoint.x&&Ny===startPoint.y)
-    {
+    else if(Nx===startPoint.x&&Ny===startPoint.y){
         startPoint.x=-1;
         startPoint.y=-1;
-    }
+    }//проверка на перекрашивание старта или финиша,  и сброс соответствующей координаты
 
     colorSquare='darkblue';
-    matrixA[Ny][Nx]=0;
+    matrixA[Ny][Nx]=0; //клетка становится непроходимой - 0
     ctxA.fillStyle=colorSquare;
-    ctxA.fillRect(Nx*pxvalue+1,Ny*pxvalue+1,pxvalue-2,pxvalue-2)
+    ctxA.fillRect(Nx*pxvalue+1,Ny*pxvalue+1,pxvalue-2,pxvalue-2) //закращиваем
 }
-else if(typeA==='impassable'&&matrixA[Ny][Nx]===0){
-    
-    
-    matrixA[Ny][Nx]=1;
-    ctxA.clearRect(Nx*pxvalue+1,Ny*pxvalue+1,pxvalue-2,pxvalue-2);
+else if(typeA==='impassable'&&matrixA[Ny][Nx]===0){//если клетка непроходима
+    matrixA[Ny][Nx]=1;//клетка становится проходимой
+    ctxA.clearRect(Nx*pxvalue+1,Ny*pxvalue+1,pxvalue-2,pxvalue-2);//стираем цвет проходимой клетки
     
 }
 console.log(startPoint.x+" "+startPoint.y);
@@ -104,6 +89,7 @@ console.log(finishPoint.x+" "+finishPoint.y);
 
 
 function clearA(){ctxA.clearRect(0,0,size,size);}
+//полностю чистим canvas
 
 function paintingA()
 {
@@ -112,7 +98,7 @@ function paintingA()
    
     var N=document.getElementById("nA");
     N=N.value;
-    
+    //получилиN
 for (var i=0;i<N;i++)
 {
     matrixA[i]=new Array;
@@ -121,7 +107,7 @@ for (var i=0;i<N;i++)
         matrixA[i][j]=1;
     }
 }
-
+//создаём матрицу , после получения N
 startPoint.x=-1;
 startPoint.y=-1;
 finishPoint.x=-1;
@@ -131,7 +117,7 @@ finishPoint.y=-1;
 
     canvasA.width=Fullpxval;
     size =canvasA.height=Fullpxval;
-
+    //изменяем размеры canvas'a, чтобы не было белых линий
     clearA()
     ctxA.beginPath();
     
@@ -146,6 +132,6 @@ finishPoint.y=-1;
         ctxA.lineTo(Fullpxval,i);
         ctxA.stroke();
     }
-
+//рисование линий по вертикали и горизонтали
 }
 
