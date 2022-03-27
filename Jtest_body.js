96fd6ae0b8554b95269addc78225fb03ef3910de
Nikita Panfilov <npanfilov2003@gmail.com>
2022-03-27 13:58:51 +0700

    Bugfix

--- Jtest_body.js
+++ Jtest_body.js
@@ -133,3 +133,59 @@ finishPoint.y=-1;
 //рисование линий по вертикали и горизонтали
 }
 
+function Point(x,y){
+    this.x=x;
+    this.y=y;
+}
+
+function searchPath(n, matrix, startPoint, finishPoint){
+    // В матрице: 0 - стена, 1 - проход
+    let queue=[];
+    let prevPoint=new Array(n*n);
+    prevPoint.fill({x:-1,y:-1});
+    queue.push(startPoint);
+    let current;
+
+    while(queue.length!=0){
+        current=queue.shift();
+        if(equal(current,finishPoint)){
+            let stack=[];
+            while(current!=startPoint){
+                stack.push(current);
+                current=prevPoint[current];
+            }
+            stack.push(current);
+            return stack;
+        }
+        adjacentCells(current,n).forEach(function(nextPoint){
+            if(equal(prevPoint[current],nextPoint) && matrix[nextPoint.x][nextPoint.y]==1){
+                queue.push(nextPoint);
+                prevPoint[nextPoint]=current;
+            }
+        });
+    }
+}
+
+function adjacentCells(point,n){
+    let array = [];
+    if (point.x>0){
+        array.push({x:point.x-1,y:point.y});
+    }
+    if(point.x+1<n){
+        array.push({x:point.x+1,y:point.y});
+    }
+    if(point.y>0){
+        array.push({x:point.x,y:point.y-1});
+    }
+    if(point.y+1<n){
+        array.push({x:point.x,y:point.y+1});
+    }
+    return array;
+}
+
+function equal(point1,point2){
+    if(point1.x==point2.x && point1.y==point2.y){
+        return true;
+    }
+    return false;
+}
