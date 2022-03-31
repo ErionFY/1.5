750a81a8c4db6da4cc827caea78be71aa303d70e
ErionFY <goncharov.timofej@gmail.com>
2022-04-01 02:50:10 +0700

    main

--- GeneticAlgorithm.js
+++ GeneticAlgorithm.js
@@ -8,17 +8,22 @@ var Vertices = []; //массив вершин с координатами
 function CreatePoint(event) {
     var xCord = event.offsetX;
     var yCord = event.offsetY;
+
     Vertices.push({ x: xCord, y: yCord });
+
     ctxGA.beginPath();
     ctxGA.arc(xCord, yCord, RadiusOfPoint, 0, 2 * Math.PI);
     ctxGA.fillStyle = 'green';
     ctxGA.fill();
     ctxGA.fillStyle = 'white';
     ctxGA.font = "20px Arial";
+
     if (Vertices.length < 10)
         ctxGA.fillText((Vertices.length).toString(), xCord - 6, yCord + 7);
-    else
+    if (Vertices.length >= 10 && Vertices.length < 100)
         ctxGA.fillText((Vertices.length).toString(), xCord - 10, yCord + 7);
+    if (Vertices.length > 100)
+        ctxGA.fillText((Vertices.length).toString(), xCord - 16, yCord + 7);
 }
 
 function clearGenetic() {
